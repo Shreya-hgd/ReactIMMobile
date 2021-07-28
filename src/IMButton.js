dafdca7339f41fda961f681cecdc7e109421ae3e
Shreya <shreya@zkteco.in>
2021-07-28 12:55:14 +0530

    First Commit Message

---
+++ src/IMButton.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Container, Content, Button, Text } from 'native-base';
+
+function IMButton() {
+    return (
+        <Container>
+        <Content>
+          <Button success>
+            <Text>IM Button</Text>
+          </Button>
+        </Content>
+      </Container>
+    )
+}
+
+export default IMButton
